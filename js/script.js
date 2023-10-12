d3eb5b6153fb7427e66f37194ace3e1b59e84552
Vinffs <vin.demiranda@gmail.com>
2023-10-12 18:25:09 +0200

    added roll dice game

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// EMAIL VALIDATOR
 const checkBtn = document.querySelector('.checkButton');
 
 checkBtn.addEventListener('click', function () {
@@ -31,3 +32,47 @@ checkBtn.addEventListener('click', function () {
   document.querySelector('.commentContainer').appendChild(email);
 }
 )
+
+
+// ROLL THE DICE
+
+// Player name 
+let player1 = "Player 1";
+let player2 = "Player 2";
+
+// Function to change the player name 
+function editNames() {
+  player1 = prompt("Change Player1 name");
+  player2 = prompt("Change player2 name");
+
+  document.querySelector("p.Player1").innerHTML = player1;
+  document.querySelector("p.Player2").innerHTML = player2;
+}
+
+// Function to roll the dice 
+function rollTheDice() {
+
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector(".img1").setAttribute("src",
+    "img/dice" + randomNumber1 + ".png");
+
+  document.querySelector(".img2").setAttribute("src",
+    "img/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
+  else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML
+      = (player2 + " WINS!");
+  }
+
+  else {
+    document.querySelector("h1").innerHTML
+      = (player1 + " WINS!");
+  }
+}
+
